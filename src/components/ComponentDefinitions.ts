1de95d51a0d0481c4468439545b8b595d8388b2c
Sam Carlberg <sam@slfc.dev>
2023-05-06 09:47:23 -0700

    Add PWM motors and quadrature encoder support

--- src/components/ComponentDefinitions.ts
+++ src/components/ComponentDefinitions.ts
@@ -4,6 +4,8 @@ import { MOTOR_CONTROLLER_GROUP } from "./definitions/MotorControllerGroup";
 import { DIFFERENTIAL_DRIVE } from "./definitions/DifferentialDrive";
 import { PID_CONTROLLER } from "./definitions/PIDController";
 import { ANALOG_GYRO } from "./definitions/AnalogGyro";
+import { QUADRATURE_ENCODER } from "./definitions/QuadratureEncoder";
+import { PWM_SPARK_MAX, PWM_TALON_FX, PWM_TALON_SRX, PWM_VICTOR_SPX, SPARK, TALON, VICTOR_SP } from "./definitions/PWMMotors";
 
 export class ComponentDefinitions {
   definitions: ComponentDefinition[] = [];
@@ -29,9 +31,81 @@ export const COMPONENT_DEFINITIONS = new ComponentDefinitions();
 
 // Seed the default values
 COMPONENT_DEFINITIONS.addDefinition(ANALOG_GYRO);
+COMPONENT_DEFINITIONS.addDefinition(QUADRATURE_ENCODER);
 
 COMPONENT_DEFINITIONS.addDefinition(CAN_TALON_FX);
+COMPONENT_DEFINITIONS.addDefinition(PWM_TALON_FX);
+COMPONENT_DEFINITIONS.addDefinition(TALON);
+COMPONENT_DEFINITIONS.addDefinition(PWM_TALON_FX);
+COMPONENT_DEFINITIONS.addDefinition(PWM_TALON_SRX);
+COMPONENT_DEFINITIONS.addDefinition(SPARK);
+COMPONENT_DEFINITIONS.addDefinition(PWM_SPARK_MAX);
+COMPONENT_DEFINITIONS.addDefinition(VICTOR_SP);
+COMPONENT_DEFINITIONS.addDefinition(PWM_VICTOR_SPX);
 COMPONENT_DEFINITIONS.addDefinition(MOTOR_CONTROLLER_GROUP);
 COMPONENT_DEFINITIONS.addDefinition(DIFFERENTIAL_DRIVE);
 
 COMPONENT_DEFINITIONS.addDefinition(PID_CONTROLLER);
+
+export function PwmMotorController({ id, name, className, fqn }: { id: string, name: string, className: string, fqn: string }): ComponentDefinition {
+  return {
+    id: id,
+    name: name,
+    type: "actuator",
+    className: className,
+    fqn: fqn,
+    wpilibApiTypes: ["motorcontroller"],
+    hints: ["action", "motor"],
+    properties: [
+      {
+        name: "PWM Channel",
+        codeName: "channel",
+        type: "int",
+        description: `The PWM Channel on the roboRIO that the ${ name } is plugged into.`,
+        setInConstructor: true
+      },
+      {
+        name: "Inverted",
+        codeName: "inverted",
+        type: "boolean",
+        description: "Whether the motor is inverted (clockwise-positive) or not (counterclockwise-positive)",
+        setInConstructor: false,
+        setter: {
+          name: "Set Inverted",
+          codeName: "setInverted",
+          description: "Sets the motor to be inverted",
+          hints: [],
+          returns: "void",
+          parameters: [
+            {
+              name: "Inverted",
+              codeName: "inverted",
+              description: "",
+              type: "boolean"
+            }
+          ]
+        }
+      }
+    ],
+    methods: [
+      {
+        name: "Set Speed",
+        description: "Sets the speed of the motor as a value from -1 for full reverse to +1 for full forward speed. The actual speed of the motor will depend on the torque load and the voltage supplied by the battery.",
+        codeName: "set",
+        hints: ["action", "motor-input"],
+        parameters: [
+          { name: "Speed", description: "", codeName: "speed", type: "double" }
+        ],
+        returns: "void"
+      },
+      {
+        name: "Stop",
+        description: "Stops the motor. Equivalent to setting the speed to 0",
+        codeName: "stopMotor",
+        hints: ["action"],
+        parameters: [],
+        returns: "void"
+      }
+    ]
+  }
+}
---
+++ src/components/definitions/QuadratureEncoder.ts
@@ -0,0 +1,115 @@
+import { ActionTemplate, ComponentDefinition } from "../ComponentDefinition";
+
+export const QUADRATURE_ENCODER: ComponentDefinition = {
+  id: "SAMPLE-quadratureencoder",
+  name: "Quadrature Encoder",
+  fqn: "edu.wpi.first.wpilibj.Encoder",
+  className: "Encoder",
+  wpilibApiTypes: [],
+  type: "sensor",
+  hints: ["state"],
+  properties: [
+    {
+      name: "Channel A",
+      codeName: "channelA",
+      type: "int",
+      description: "The digital input channel for the A channel",
+      setInConstructor: true
+    },
+    {
+      name: "Channel B",
+      codeName: "channelB",
+      type: "int",
+      description: "The digital input channel for the B channel",
+      setInConstructor: true
+    },
+    {
+      name: "Reversed",
+      codeName: "reverse",
+      type: "boolean",
+      description: "Flags the encoder as reversed so that 'forward' motion is indicated by positive values, however you choose to define 'forward'",
+      setInConstructor: false, // optional parameter, and we have a setter...
+      setter: {
+        name: "Set Reversed",
+        codeName: "setReverseDirection",
+        description: "Flags the encoder as reversed so that 'forward' motion is indicated by positive values, however you choose to define 'forward'",
+        hints: ["housekeeping"],
+        parameters: [
+          {
+            name: "Reversed",
+            codeName: "reverseDirection",
+            type: "boolean",
+            description: "" // eh, seems redundant
+          }
+        ],
+        returns: "void"
+      }
+    },
+    {
+      name: "Distance per Pulse",
+      codeName: "distancePerPulse",
+      type: "double",
+      description: "How far the mechanism moves per encoder pulse. \
+                    This is a unitless value and can represent either linear or rotational distance. \
+                    It is up to you, the programmer, to keep track of the units",
+      setInConstructor: false,
+      setter: {
+        name: "Set Distance per Pulse",
+        codeName: "setDistancePerPulse",
+        description: "Sets how far the mechanism moves per encoder pulse",
+        hints: ["configuration"],
+        parameters: [
+          {
+            name: "Distance per Pulse",
+            codeName: "distancePerPulse",
+            type: "double",
+            description: ""
+          }
+        ],
+        returns: "void"
+      },
+    }
+  ],
+  methods: [
+    {
+      name: "Get Count",
+      codeName: "get",
+      description: "Gets the current encoder count from the sensor",
+      hints: ["state"],
+      parameters: [],
+      returns: "int"
+    },
+    {
+      name: "Reset Count",
+      codeName: "reset",
+      description: "Resets the current count to zero",
+      hints: ["housekeeping"],
+      parameters: [],
+      returns: "void"
+    },
+    {
+      name: "Get Distance",
+      codeName: "getDistance",
+      description: "Gets the travelled distance of the encoder since the last reset. Distance units are based on the value set for Distance per Pulse, or raw encoder units if it was not set.",
+      hints: ["state"],
+      parameters: [],
+      returns: "double"
+    },
+    {
+      name: "Get Rate",
+      codeName: "getRate",
+      description: "Gets the current velocity of the encoder. Returns in units of distance per second, where the distance unit depends on what you set for Distance per Pulse. If that was not set, distance units are raw encoder ticks.",
+      hints: ["state"],
+      parameters: [],
+      returns: "double"
+    },
+    {
+      name: "Stopped",
+      codeName: "getStopped",
+      description: "Checks if the encoder has stopped moving",
+      hints: ["state"],
+      parameters: [],
+      returns: "boolean"
+    }
+  ],
+}
