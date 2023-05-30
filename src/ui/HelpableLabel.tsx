72e4c4219d8631ea878e615ba14f757a21d86743
Sam Carlberg <sam@slfc.dev>
2023-05-30 19:12:47 -0400

    Add HelpableLabel component
    Provides a tooltip for displaying extra detail about something

---
+++ src/ui/HelpableLabel.tsx
@@ -0,0 +1,25 @@
+import { HelpOutlineOutlined } from "@mui/icons-material";
+import { Tooltip } from "@mui/material";
+import React, { useState } from "react";
+
+type HelpableLabelProps = {
+  description: string
+  children: React.ReactNode
+}
+
+export function HelpableLabel({ description, children }: HelpableLabelProps) {
+  const [showIcon, setShowIcon] = useState(false);
+
+  return (
+    <span onMouseEnter={ () => setShowIcon(true) } onMouseLeave={ () => setShowIcon(false) } className="helpable-label">
+      <span className="helpable-label-text">
+        {
+          children
+        }
+      </span>
+      <Tooltip title={ description }>
+        <HelpOutlineOutlined className={ `helpable-label-icon ${ showIcon ? 'visible' : 'invisible' }` } />
+      </Tooltip>
+    </span>
+  )
+}
